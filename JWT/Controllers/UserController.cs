9f941c0b886866e1a0791a5cb72fda219e3622ef
Anik Shahrier Oishik <anikshahrieroishik@gmail.com>
2022-02-03 04:44:43 +0600

    Learnt Dependent Injection Method, Constructor Injection Method

---
+++ JWT/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using JWT.BindingModel;
+using JWT.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+
+        private readonly ILogger<UserController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+
+        public UserController(ILogger<UserController> logger,UserManager<AppUser> userManager, SignInManager<AppUser> signManager)
+        {
+            userManager = userManager;
+            signManager = signManager;
+            _logger = logger;
+        }
+
+        public async Task<object> RegisterUser([FromBody] AddUpdateRegisterUserBindingModel model) {
+
+            var user = new AppUser() { FullName = model.FullName, Email = model.Email, DateCreated = DateTime.UtcNow, DateModified = DateTime.UtcNow };
+
+
+             var result = await _userManager.CreateAsync(user, model.Password);
+            if(result.Succeeded)
+            {
+                return await Task.FromResult("Thank You for registering to help me");
+            }
+            return await Task.FromResult(string.Join(",",result.Errors.Select(x=>x.Description.ToArray())));
+
+        }
+
+       
+    }
+}
--- JWT/Startup.cs
+++ JWT/Startup.cs
@@ -1,7 +1,11 @@
+using JWT.Data;
+using JWT.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +30,11 @@ namespace JWT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDBContext>(opt => {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                });
 
+          services.AddIdentity<AppUser, IdentityRole>(opt => { }).AddEntityFrameworkStores<AppDBContext>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,6 +51,7 @@ namespace JWT
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWT v1"));
             }
+
 
             app.UseHttpsRedirection();
 

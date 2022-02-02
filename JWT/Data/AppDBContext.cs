8a9a4ecc656fca4b403f438919750c9ef280dfef
Anik Shahrier Oishik <anikshahrieroishik@gmail.com>
2022-02-03 00:14:13 +0600

    Database Added

---
+++ JWT/Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using JWT.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Data
+{
+    public class AppDBContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
